f381ca4ac751b7a9b0e091df52161201d1ca4b20
Pavol Juhas <pavol.juhas@gmail.com>
2014-02-06 19:03:23 -0500

    Remove FPATH and PATH checks.
    oh-my-zsh should have fpath already set to the plugin directory.

--- plugins/scd/scd.plugin.zsh
+++ plugins/scd/scd.plugin.zsh
@@ -1,13 +1,5 @@
-## Try to load scd as a shell function either from FPATH or from PATH
-## if installed at either place.  Complain if scd cannot be found.
-if ( found=( ${^fpath}/scd(N-.) ) && [[ -n ${found} ]] ); then
-    autoload scd
-elif fpath=( ${fpath} =scd(:h) ); then
-    autoload +X scd || unfunction -m scd
-    fpath[-1]=( )
-else
-    print -u2 "scd could not be found neither in FPATH nor in PATH."
-fi
+## The scd script should autoload as a shell function.
+autoload scd
 
 
 ## If the scd function exists, define a change-directory-hook function
